c6dfb60a2e1a22bd1175c97d506bbf06d301525c
aldogerard <aldogerard12@gmail.com>
2022-08-16 15:36:09 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>     Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim consectetur asperiores nam, error quidem ad harum reiciendis pariatur voluptates distinctio vitae, perferendis nisi aut nobis sint, quos vel culpa laborum.</p>
 </body>
 </html>
